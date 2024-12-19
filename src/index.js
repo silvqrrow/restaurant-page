f15e371ac110ff9dd77df3727eba8c857490fb1d
silvqrrow <105092230+silvqrrow@users.noreply.github.com>
2024-12-18 20:21:04 -0500

    Basic HTML and CSS styling for home

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
 import "./styles.css";
+
+const content = document.querySelector("#content");
+const year = document.createElement("p");
+const heading = document.createElement("h1");
+const line = document.createElement("hr");
+const location = document.createElement("p");
+
+year.textContent = "1582";
+heading.textContent = "La Tour d'Argent";
+location.textContent = "Paris";
