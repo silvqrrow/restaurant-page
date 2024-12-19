3941bdb8dadca768103f88181bacdcdb16c02387
silvqrrow <105092230+silvqrrow@users.noreply.github.com>
2024-12-18 20:42:28 -0500

    Add homepage rendering

--- src/index.js
+++ src/index.js
@@ -1,4 +1,14 @@
 import "./styles.css";
 import { homePage } from "./homepage.js";
 
+const home = document.querySelector("#home");
+const menu = document.querySelector("#menu");
+const about = document.querySelector("#about");
 homePage();
+
+home.addEventListener("click", () => {
+  while (content.firstChild) {
+    content.removeChild(content.lastChild);
+  }
+  homePage();
+});
